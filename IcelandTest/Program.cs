74c38ad8bcf1147c72649ab72db336ac171c4248
Tat <dctat13@gmail.com>
2021-07-16 09:50:18 +0100

    Init

---
+++ IcelandTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IcelandTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
