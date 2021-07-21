2151146645041b228305cdf06d9abc03097fbd5f
Tat <dctat13@gmail.com>
2021-07-21 17:27:46 +0100

    Code for running the program once a day at the end of the day

--- IcelandTest/Program.cs
+++ IcelandTest/Program.cs
@@ -7,8 +7,11 @@ namespace IcelandTest
     {
         static async Task Main(string[] args)
         {
-            string input =
-@"Aged Brie 1 1
+
+            while (true)//outer while loop
+            {
+                string input =
+    @"Aged Brie 1 1
 Christmas Crackers -1 2
 Christmas Crackers 9 2
 Soap 2 2
@@ -19,17 +22,26 @@ Fresh Item 2 2
 Fresh Item -1 5";
 
 
+                //Code for delay run until 23:59:59, comment this section and the outer while loop if trail run
+                var runAt = new TimeSpan(23, 59, 59);
 
-            InventoryManagement inventoryManagement = new InventoryManagement();
+                int msToNextRun = (int)runAt.Subtract(DateTime.Now.TimeOfDay).TotalMilliseconds;
+                await Task.Delay(msToNextRun);
+                //End of Code for delay run until 23:59:59
 
-            foreach (string line in input.Split(Environment.NewLine))
-            {
-                var result = InputRecognition.Process(line);
-                inventoryManagement.Run(result);
+
+
+                InventoryManagement inventoryManagement = new InventoryManagement();
+
+                foreach (string line in input.Split(Environment.NewLine))
+                {
+                    var result = InputRecognition.Process(line);
+                    inventoryManagement.Run(result);
+                }
+
             }
 
-            Console.ReadLine();
-
+            Console.ReadKey();
         }
     }
 }
