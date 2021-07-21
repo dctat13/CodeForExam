c718a699ca9a222ee605e1b6fe2b8a9810ebe61b
Tat <dctat13@gmail.com>
2021-07-21 12:12:55 +0100

    Add Product models

---
+++ IcelandTest/Factory/AgedBrieFactory.cs
@@ -0,0 +1,24 @@
+﻿using IcelandTest.Interfaces;
+using IcelandTest.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IcelandTest.Factory
+{
+    public class AgedBrieFactory: IProductFactory
+    {
+        public IProduct Create(string label, int sellIn, int quality)
+        {
+            return new AgedBrie()
+            {
+                Label = label,
+                SellIn = sellIn,
+                Quality = quality,
+                ProductType = ProductType.FrozenItem,
+            };
+        }
+    }
+}
