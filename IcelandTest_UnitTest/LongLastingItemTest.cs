64b1094052834b582d8106871866f41fb4a417f5
Tat <dctat13@gmail.com>
2021-07-21 16:48:30 +0100

    Unit test for SOAP

--- IcelandTest_UnitTest/LongLastingItemTest.cs
+++ IcelandTest_UnitTest/LongLastingItemTest.cs
@@ -1,4 +1,5 @@
-﻿using IcelandTest.Interfaces;
+﻿using IcelandTest.Factory;
+using IcelandTest.Interfaces;
 using IcelandTest.Models;
 using IcelandTest.Processes;
 using NUnit.Framework;
@@ -16,6 +17,70 @@ namespace IcelandTest_UnitTest
         public void Setup()
         {
         }
+
+        [Test]
+        public void AgedBrieFactory_EqualPropertyValuesAsDefined()
+        {
+            //assign
+            IProductFactory factory = new LongLastItemFactoy();
+            string label = "Soap";
+            int sellIn = 1;
+            int quality = 1;
+
+            //test
+            var product = factory.Create(label, sellIn, quality);
+
+            //result
+            Assert.AreEqual(label, product.Label);
+            Assert.AreEqual(sellIn, product.SellIn);
+            Assert.AreEqual(quality, product.Quality);
+        }
+
+        [Test]
+        public void AgedBrieFactory_NotEqualPropertyValuesAsDefined()
+        {
+            //assign
+            IProductFactory factory = new LongLastItemFactoy();
+            string label = "Soap";
+            int sellIn = 1;
+            int quality = 1;
+
+            //test
+            var product = factory.Create(label, sellIn, quality);
+
+            //result
+            Assert.AreNotEqual(label + "123", product.Label);
+            Assert.AreNotEqual(++sellIn, product.SellIn);
+            Assert.AreNotEqual(++quality, product.Quality);
+        }
+
+
+        [Test]
+        public void AgedBrieFactory_QualitySellinNeverChange()
+        {
+            //assign
+            IProductFactory factory = new LongLastItemFactoy();
+            string label = "Soap";
+            int sellIn = 1;
+            int quality = 1;
+            IProductProcess process = new LongLastingItemProcess();
+
+            var product = new LongLastingItem()
+            {
+                Label = label,
+                ProductType = ProductType.LongLastingItem,
+                Quality = quality,
+                SellIn = sellIn
+            };
+
+            //test
+            process.Execute(product);
+
+
+            //result
+            Assert.AreEqual(quality, product.Quality);
+        }
+
 
     }
 }
