cfdf863f429278f26d0e723ec00b7e6b8ce342b5
Tat <dctat13@gmail.com>
2021-07-21 12:22:55 +0100

    Prepare Main

--- IcelandTest/Program.cs
+++ IcelandTest/Program.cs
@@ -1,12 +1,33 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 namespace IcelandTest
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string input =
+@"Aged Brie 1 1
+Christmas Crackers -1 2
+Christmas Crackers 9 2
+Soap 2 2
+Frozen Item -1 55
+Frozen Item 2 2
+INVALID ITEM 2 2
+Fresh Item 2 2
+Fresh Item -1 5";
+
+
+            foreach (string line in input.Split(Environment.NewLine))
+            {
+                var result = InputRecognition.Process(line);
+
+
+            }
+
+            Console.ReadLine();
+
         }
     }
 }
