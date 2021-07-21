c8610905a6732d2ad3c018d7dd262d28b0b3bbb7
Tat <dctat13@gmail.com>
2021-07-21 12:35:17 +0100

    Aged Brie Process

---
+++ IcelandTest/Processes/AgedBrieProcess.cs
@@ -0,0 +1,17 @@
+﻿using IcelandTest.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IcelandTest.Processes
+{
+    public class AgedBrieProcess : ProductProcess
+    {
+        protected override void UpdateQuality(IProduct product)
+        {
+            product.Quality += 1;
+        }
+    }
+}
