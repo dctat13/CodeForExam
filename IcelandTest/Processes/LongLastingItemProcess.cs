0a8c8cfdf4164b937b6b0afe0b9ecd8a3452d007
Tat <dctat13@gmail.com>
2021-07-21 15:35:52 +0100

    long lasting item process

---
+++ IcelandTest/Processes/LongLastingItemProcess.cs
@@ -0,0 +1,22 @@
+﻿using IcelandTest.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IcelandTest.Processes
+{
+    public class LongLastingItemProcess : ProductProcess
+    {
+        protected override void UpdateQuality(IProduct product)
+        {
+            //no degrading
+        }
+
+        protected override void UpdateSellIn(IProduct product)
+        {
+            //no limitation
+        }
+    }
+}
