4a49c2deab2f2c29d3e891175c24228b2d471e1b
Tat <dctat13@gmail.com>
2021-07-21 12:34:37 +0100

    Abstract process

---
+++ IcelandTest/Processes/ProductProcess.cs
@@ -0,0 +1,51 @@
+﻿using IcelandTest.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IcelandTest.Processes
+{
+    public abstract class ProductProcess : IProductProcess
+    {
+        
+        protected virtual void UpdateSellIn(IProduct product)
+        {
+            product.SellIn -= 1;
+        }
+
+        protected virtual void UpdateQuality(IProduct product)
+        {
+            product.Quality -= 1;
+        }
+
+        public virtual void Execute(IProduct product)
+        {
+
+            UpdateQuality(product);
+            if (product.SellIn < 0)
+            {
+                //Once the sell by date has passed, Quality degrades twice as fast 
+                UpdateQuality(product);
+            }
+
+
+            UpdateSellIn(product);
+
+            ValidateQuality(product);
+        }
+
+        public void ValidateQuality(IProduct product)
+        {
+            if (product.Quality > 50)
+            {
+                product.Quality = 50;
+            }
+            if (product.Quality < 0)
+            {
+                product.Quality = 0;
+            }
+        }
+    }
+}
