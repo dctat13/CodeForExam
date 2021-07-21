b9de535399fddfb484956c4ed6bc05e979220e0c
Tat <dctat13@gmail.com>
2021-07-21 15:42:10 +0100

    Fresh Item process

---
+++ IcelandTest/Processes/FreshItemProcess.cs
@@ -0,0 +1,18 @@
+﻿using IcelandTest.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IcelandTest.Processes
+{
+    public class FreshItemProcess : FrozenItemProcess
+    {
+        protected override void UpdateQuality(IProduct product)
+        {
+            base.UpdateQuality(product);
+            base.UpdateQuality(product);
+        }
+    }
+}
