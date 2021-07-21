c05493eff53701c379593d6847cf7386bdef4b92
Tat <dctat13@gmail.com>
2021-07-21 12:07:47 +0100

    Add Product models

---
+++ IcelandTest/Models/FrozenItem.cs
@@ -0,0 +1,17 @@
+﻿using IcelandTest.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IcelandTest.Models
+{
+    public class FrozenItem : IProduct
+    {
+        public string Label { get; set; }
+        public int SellIn { get; set; }
+        public int Quality { get; set; }
+        public ProductType ProductType { get; set; }
+    }
+}
